1b708fb3e9fe69657dbd75b21ab2ddff7988f819
melok08 <koblankossonou@gmail.com>
2024-07-12 06:06:32 +0000

    Mise a jour

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -63,7 +63,7 @@ function Form() {
   };
 
   return (
-    <div >
+    <div className={styles.contain} >
       <form id={styles.msform} onSubmit={handleSubmit}>
         {/* Progressbar */}
         <ul id={styles.progressbar}>
