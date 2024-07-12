08c70300fc5ade9d76bafe731d3c65a787cb4faa
melok08 <koblankossonou@gmail.com>
2024-07-12 04:27:25 +0000

    Mise a jour

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,30 +1,39 @@
-import React from 'react'
-import './Header.css'
-import Logo from '../logo/Logo'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import styles from './Header.module.css'; // Importez les styles du module CSS
+import Logo from '../logo/Logo';
+import { Link } from 'react-router-dom';
 
 function Header() {
-    return (
-        <div className='header'>
-              <div className='logo'>
-                    <Logo/>
-                </div>
-            <div className='navbar'>
-              <ul>
-                
-                <Link to="/">
-                  <li><a href="">Accueil</a></li> 
-                </Link>
+  const [isOpen, setIsOpen] = useState(false);
 
-                <Link to="/inscription">
-                   <li><a href="">Inscription</a></li>  
-                </Link>
-               
-                
-              </ul>
-            </div>
-        </div>
-    )
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className={styles.header}> {/* Utilisation du nom de classe généré par le module CSS */}
+      <div className={styles.logo}>
+        <Logo />
+      </div>
+      <div className={`${styles.navbar} ${isOpen ? styles.open : ''}`}>
+        <ul>
+          <li>
+            <Link to='/'>
+              <a href=''>Accueil</a>
+            </Link>
+          </li>
+          <li>
+            <Link to='/inscription'>
+              <a href=''>Inscription</a>
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div className={styles['menu-icon']} onClick={toggleMenu}>
+        &#9776; {/* Icône hamburger */}
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
