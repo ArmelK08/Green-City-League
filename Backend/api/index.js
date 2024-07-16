9372375bab669c5a20b7a26e4fe30914746529ba
ArmelK08 <78016519+ArmelK08@users.noreply.github.com>
2024-07-16 00:28:30 +0000

    Update index.js

--- Backend/api/index.js
+++ Backend/api/index.js
@@ -10,10 +10,15 @@ const corsOptions = {
   origin: 'https://gcl-indol.vercel.app', // Remplacez par l'URL de votre frontend
   optionsSuccessStatus: 200,
 };
-
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
 app.use(cors(corsOptions));
 
-mongoose.connect('mongodb+srv://koblankossonou:melok0810@cluster0.ecsz1gb.mongodb.net/?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://koblankossonou:koblankossonou@cluster0.ecsz1gb.mongodb.net/?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
